6e8d198cd0fcb5a4cc0e0ef634fcc6cc66710201
Luke Curley <kixelated@gmail.com>
2026-02-20 14:30:53 -0800

    Abort the IETF publisher on session close. (#981)

--- rs/moq-lite/src/ietf/publisher.rs
+++ rs/moq-lite/src/ietf/publisher.rs
@@ -189,6 +189,7 @@ impl<S: web_transport_trait::Session> Publisher<S> {
 			let group = tokio::select! {
 				biased;
 				_ = &mut cancel => return Ok(()),
+				_ = session.closed() => return Err(Error::Transport),
 				Some(group) = track.next_group().transpose() => group,
 				Some(_) = async { Some(old_group.as_mut()?.await) } => {
 					old_group = None;
